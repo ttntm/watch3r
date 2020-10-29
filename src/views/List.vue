bcdc087db0835ec1352a1363d573c5f5d58c1896
Tom Doe <ttntm@pm.me>
2020-10-29 20:53:56 +0100

    Update List.vue

--- src/views/List.vue
+++ src/views/List.vue
@@ -35,7 +35,7 @@
     <transition name="overlay">
       <div v-if="addModalOpen || editModalOpen || posterModalOpen" class="overlay" />
     </transition>
-    <BtnToTop v-if="!addModalOpen || !editModalOpen || !posterModalOpen" />
+    <BtnToTop v-if="!addModalOpen && !editModalOpen && !posterModalOpen" />
   </section>
 </template>
 
