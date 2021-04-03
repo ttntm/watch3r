ab3b771018076b1ccff3354d22567e5dd0d863d6
ttntm <ttntm@pm.me>
2021-04-04 01:51:29 +0200

    update: 404 ts

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  build: {
+    cssCodeSplit: false
+  },
   optimizeDeps: {
     include: [
       'body-scroll-lock',
