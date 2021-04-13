2c1e135d05da883466dd51220f847360f7c4958d
ttntm <ttntm@pm.me>
2021-04-13 21:00:02 +0200

    add: link to GitHub Discussions

--- src/views/Support.vue
+++ src/views/Support.vue
@@ -10,7 +10,10 @@
       &gt; How Can I Contact You?
     </h2>
     <p>
-      You can send us an email at <a href="mailto:hello@watch3r.app">hello@watch3r.app</a>
+      You can send us an email at <a href="mailto:hello@watch3r.app">hello@watch3r.app</a>.
+    </p>
+    <p>
+      There's also <a href="https://github.com/ttntm/watch3r/discussions" target="_blank" rel="noopener">GitHub Discussions</a> for general questions and feedback.
     </p>
     <h2>
       &gt; I Found A Bug...
@@ -19,7 +22,7 @@
       Ooops, we're sorry :(
     </p>
     <p>
-      Please head over to <a href="https://github.com/ttntm/watchlist/issues" target="blank">GitHub</a> and report it there. You can also report it via email if that's more convenient for you.
+      Please head over to <a href="https://github.com/ttntm/watchlist/issues" target="blank" rel="noopener">GitHub</a> and report it there. You can also report it via email if that's more convenient for you.
     </p>
     <p>
       If possible, please include some basic information like the browser you used, steps to reproduce the issue and screenshots.
@@ -28,7 +31,7 @@
       &gt; Feature Requests
     </h2>
     <p>
-      Same as for bugs - please head over to <a href="https://github.com/ttntm/watchlist/issues" target="blank">GitHub</a> and file it there or send us an email.
+      Same as for bugs - please head over to <a href="https://github.com/ttntm/watchlist/issues" target="blank" rel="noopener">GitHub</a> and file it there or send us an email.
     </p>
     <h2>
       &times; Account Removal
