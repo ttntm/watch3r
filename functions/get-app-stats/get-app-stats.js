eb6a2321c4a1f43e2cae1e9e76c643a131bff1d0
ttntm <ttntm@pm.me>
2021-04-19 22:53:32 +0200

    update: stats working

--- functions/get-app-stats/get-app-stats.js
+++ functions/get-app-stats/get-app-stats.js
@@ -14,14 +14,14 @@ exports.handler = async (event, context, callback) => {
     return callback(null, { statusCode: 405, body: 'Method Not Allowed'})
   } else {
     try {
-      // step 1: query the database for the necessary data
+      // STEP 1: query the database for the necessary data
       const getIndexItemCount = async (indexName) => {
         const req = client.query(q.Count(q.Paginate(q.Match(q.Index(`${indexName}`)), { size: 10000 })));
         let response = await req;
         return response.data[0];
       }
-
-      // step 2: write into Google Sheets for tracking
+      // STEP 2: write into Google Sheets for tracking
+      // see: https://theoephraim.github.io/node-google-spreadsheet/#/?id=examples
       const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID);
       // authenticate
       await doc.useServiceAccountAuth({
@@ -29,18 +29,19 @@ exports.handler = async (event, context, callback) => {
         private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
       });
       // initialize sheet
-      await doc.loadInfo(); // loads document properties and worksheets. required.
-      const sheet = doc.sheetsByIndex[0]; // you may want to customize this if you have more than 1 sheet
-
+      await doc.loadInfo(); // loads document properties and worksheets; required.
+      const sheet = doc.sheetsByIndex[0]; // use the first sheet
       // add the query results to the sheet
       const now = new Date();
+      // DO NOT use 'lists.forEach()' here => see: https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
+      // if you do, unpredicatable things will happen and data will most likely never show up in Google Sheets due to 'await' calls never coming back after being triggered
       for (const listname of lists) {
         const addedRow = await sheet.addRow({
           Date: date.format(now, 'DD.MM.YYYY'),
           List: listname,
           ListItemCount: await getIndexItemCount(`${listname}_all`)
         });
-        console.log(addedRow._rowNumber, addedRow._rawData);
+        console.log(addedRow._rowNumber, addedRow._rawData); // visible in Netlify's funcion logs
       }
 
       return {
