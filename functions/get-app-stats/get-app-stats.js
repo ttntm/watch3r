4a374d88c9b82e756db5f82aa239168d145f3ad6
ttntm <ttntm@pm.me>
2021-04-19 21:40:52 +0200

    update: stats function

--- functions/get-app-stats/get-app-stats.js
+++ functions/get-app-stats/get-app-stats.js
@@ -5,29 +5,20 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 const lists = ['tracklist','watchlist'];
 
 exports.handler = async (event, context, callback) => {
+  const client = new faunadb.Client({
+    secret: process.env.FAUNA_SECRET
+  });
+  const q = faunadb.query;
+
   if (event.httpMethod !== 'POST') {
     return callback(null, { statusCode: 405, body: 'Method Not Allowed'})
   } else {
     try {
       // step 1: query the database for the necessary data
-      const client = new faunadb.Client({
-        secret: process.env.FAUNA_SECRET
-      });
-      const q = faunadb.query;
-
       const getIndexItemCount = async (indexName) => {
         const req = client.query(q.Count(q.Paginate(q.Match(q.Index(`${indexName}`)), { size: 10000 })));
         let response = await req;
         return response.data[0];
-      }
-
-      const getSheetRowData = async (listname) => {
-        const now = new Date();
-        return {
-          Date: date.format(now, 'DD.MM.YYYY'),
-          List: listname,
-          ListItemCount: await getIndexItemCount(`${listname}_all`)
-        };
       }
 
       // step 2: write into Google Sheets for tracking
@@ -40,9 +31,15 @@ exports.handler = async (event, context, callback) => {
       // initialize sheet
       await doc.loadInfo(); // loads document properties and worksheets. required.
       const sheet = doc.sheetsByIndex[0]; // you may want to customize this if you have more than 1 sheet
+
       // add the query results to the sheet
-      lists.forEach(async (list) => {
-        return await sheet.addRow(await getSheetRowData(list));
+      const now = new Date();
+      lists.forEach(async (listname) => {
+        await sheet.addRow({
+          Date: date.format(now, 'DD.MM.YYYY'),
+          List: listname,
+          ListItemCount: await getIndexItemCount(`${listname}_all`)
+        });
       });
 
       return {
