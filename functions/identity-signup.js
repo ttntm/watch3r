228b85ead5ceb0fde95dc00ad0c0bbbf122307b6
ttntm <ttntm@pm.me>
2025-04-10 09:35:43 +0200

    re: signup hook

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -3,86 +3,95 @@ const fnHeaders = require('./_shared/headers.js')
 const spb = require('@supabase/supabase-js')
 
 exports.handler = async (event, context, callback) => {
-  const { identity } = context.clientContext
-  const adminAuthHeader = `Bearer ${identity.token}`
-  const usersUrl = `${identity.url}/admin/users`
+  const rawNetlifyContext = context.clientContext.custom.netlify
+  const netlifyContext = Buffer.from(rawNetlifyContext, 'base64').toString('utf-8')
+  const { identity, user } = JSON.parse(netlifyContext)
+  const debug = { i:identity, u: user }
 
-  try {
-    // get userlist
-    const requestUL = await fetch(usersUrl, {
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": adminAuthHeader,
-      },
-    })
+  console.log(debug)
 
-    let responseUL = await requestUL.json()
-
-    if (responseUL.users.length > 0) {
-      const supabase = spb.createClient(process.env.SPB_URL, process.env.SPB_API_KEY, {
-        auth: {
-          autoRefreshToken: false,
-          detectSessionInUrl: false,
-          persistSession: false
-        }
-      })
-      let userlistUpdate = []
-
-      for (const user of responseUL.users) {
-        if (user.confirmed_at) {
-          userlistUpdate.push({
-            id: user.id,
-            email: user.email,
-            created: user.confirmed_at
-          })
-        }
-      }
-
-      try {
-        const { data, error } = await supabase
-          .from('users')
-          .upsert(userlistUpdate)
-          .select()
-
-          if (error) {
-            throw JSON.stringify(error)
-          }
-
-          return {
-            statusCode: 200,
-            headers: fnHeaders,
-            body: JSON.stringify({ message: 'Userlist updated' })
-          }
-      } catch (ex) {
-        console.error(ex)
-
-        return {
-          statusCode: 400,
-          headers: fnHeaders,
-          body: typeof ex === 'string'
-            ? ex
-            : JSON.stringify(ex)
-        }
-      }
-    } else {
-      console.error(ex)
-
-      return {
-        statusCode: 400,
-        headers: fnHeaders,
-        body: 'Could not obtain userlist'
-      }
-    }
-  } catch (ex) {
-    console.error(ex)
-
-    return {
-      statusCode: 400,
-      headers: fnHeaders,
-      body: typeof ex === 'string'
-        ? ex
-        : JSON.stringify(ex)
-    }
+  return {
+    statusCode: 200,
+    headers: fnHeaders,
+    body: JSON.stringify(debug)
   }
+
+  // try {
+  //   // get userlist
+  //   const requestUL = await fetch(usersUrl, {
+  //     method: 'GET',
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Authorization": adminAuthHeader,
+  //     },
+  //   })
+
+  //   let responseUL = await requestUL.json()
+
+  //   if (responseUL.users.length > 0) {
+  //     const supabase = spb.createClient(process.env.SPB_URL, process.env.SPB_API_KEY, {
+  //       auth: {
+  //         autoRefreshToken: false,
+  //         detectSessionInUrl: false,
+  //         persistSession: false
+  //       }
+  //     })
+  //     let userlistUpdate = []
+
+  //     for (const user of responseUL.users) {
+  //       if (user.confirmed_at) {
+  //         userlistUpdate.push({
+  //           id: user.id,
+  //           email: user.email,
+  //           created: user.confirmed_at
+  //         })
+  //       }
+  //     }
+
+  //     try {
+  //       const { data, error } = await supabase
+  //         .from('users')
+  //         .upsert(userlistUpdate)
+  //         .select()
+
+  //         if (error) {
+  //           throw JSON.stringify(error)
+  //         }
+
+  //         return {
+  //           statusCode: 200,
+  //           headers: fnHeaders,
+  //           body: JSON.stringify({ message: 'Userlist updated' })
+  //         }
+  //     } catch (ex) {
+  //       console.error(ex)
+
+  //       return {
+  //         statusCode: 400,
+  //         headers: fnHeaders,
+  //         body: typeof ex === 'string'
+  //           ? ex
+  //           : JSON.stringify(ex)
+  //       }
+  //     }
+  //   } else {
+  //     console.error(ex)
+
+  //     return {
+  //       statusCode: 400,
+  //       headers: fnHeaders,
+  //       body: 'Could not obtain userlist'
+  //     }
+  //   }
+  // } catch (ex) {
+  //   console.error(ex)
+
+  //   return {
+  //     statusCode: 400,
+  //     headers: fnHeaders,
+  //     body: typeof ex === 'string'
+  //       ? ex
+  //       : JSON.stringify(ex)
+  //   }
+  // }
 }
